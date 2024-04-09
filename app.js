c22898861a4968617588755106b0f08c2f9a9ab3
Luis Valdes <lvaldes2211@gmail.com>
2024-04-09 13:34:11 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const port = process.env.PORT || 3000;
 const app = express();
 app.use( cors() );
 app.use( express.json() )
 app.use('/mobile/clientes', require('./routes/clientes'));
-app.listen(3000, ()=>{
-    console.log("Server corriendo en puerto",3000)
+app.listen(port, ()=>{
+    console.log("Server corriendo en puerto",port)
 })
