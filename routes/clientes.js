f771a8d1b457a3e8f530a2baf26f6369d9d779f6
Luis Valdes <lvaldes2211@gmail.com>
2024-04-15 11:31:37 -0500

    Delete routes directory

--- routes/clientes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { listarClientes } = require('../controller/clientes');
-
-const router = Router();
-
-router.post('/listar/', listarClientes);
-
-module.exports = router;
